a266fa774347eefc64820eb02bc3cea38242ac7c
Gerson <gersonscolari2@gmail.com>
2026-02-27 15:31:16 -0300

    Exercicio 11 Atividade 2

--- Lista2/Exercicio11.php
+++ Lista2/Exercicio11.php
@@ -10,28 +10,22 @@
 
 <body>
     <div class="container py-3">
-        <h1>Exercício 10 - Perímetro do retângulo:</h1>
+        <h1>Exercício 11 - Perímetro do círculo:</h1>
         <form method="post">
             <div class="mb-3">
-                <label for="Largura" class="form-label">Informe a largura do retângulo: </label>
-                <input type="number" id="Largura" name="Largura" class="form-control" required="">
-            </div>
-            <div class="mb-3">
-                <label for="Altura" class="form-label">Informe a altura do retângulo: </label>
-                <input type="number" id="Altura" name="Altura" class="form-control" required="">
+                <label for="Raio" class="form-label">Informe o raio do círculo (cm): </label>
+                <input type="number" id="Raio" name="Raio" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
         <?php
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+            $Raio = $_POST['Raio'];
 
-            $Base = $_POST['Largura'];
-            $Altura = $_POST['Altura'];
+            $Perimetro = $Raio * 3.14 * 2;
 
-            $Perimetro = ($Base + $Altura) * 2;
-
-            echo "O Valor do perímetro do retângulo é de $Perimetro";
-
+            echo "O valor do perímetro do círculo é de $Perimetro (Considerando π = 3,14)";
         }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
