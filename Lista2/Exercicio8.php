6483f800732ac164923dba7e14b493da6478a947
Gerson <gersonscolari2@gmail.com>
2026-02-27 14:48:47 -0300

    Exercicio 8 Atividade 2

---
+++ Lista2/Exercicio8.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Atividade 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 8 - Area do retângulo:</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="Largura" class="form-label">Informe a largura do retângulo: </label>
+                <input type="number" id="Largura" name="Largura" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="Altura" class="form-label">Informe a altura do retângulo: </label>
+                <input type="number" id="Altura" name="Altura" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            $Base = $_POST['Largura'];
+            $Altura = $_POST['Altura'];
+
+            $Area = $Base * $Altura;
+
+            echo "A Area do Retângulo é $Area";
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
