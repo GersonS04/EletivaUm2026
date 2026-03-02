992ea971ce924ae1892d8f46e67b4ba8437f9728
Gerson <gersonscolari2@gmail.com>
2026-03-02 14:21:36 -0300

    Exercicio 14 Atividade 2

---
+++ Lista2/Exercicio14.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Atividade 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercicio 14 - Quilômetros -> Milhas</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="km" class="form-label">Informe o valor em Quilômetros:</label>
+                <input type="number" id="km" name="km" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $Km = $_POST['km'];
+
+            $Milhas = $Km * 0.621371;
+
+            echo "$Km Quilômetros correspondem a " . number_format($Milhas, 2, '.', ''). " milhas";
+
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
