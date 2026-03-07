c91d963bb182ddc16a61a04e1d570fdba2e5a01e
Gerson <gersonscolari2@gmail.com>
2026-03-07 12:07:17 -0300

    Formulário Exercício 1 - Atividade 3

---
+++ Lista3/Exercicio1.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Atividade 3</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Atividade 3</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="x1" class="form-label">Informe o primeiro valor:</label>
+              <input type="number" id="x1" name="x1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="x2" class="form-label">Informe o segundo valor:</label>
+              <input type="number" id="x2" name="x2" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="x3" class="form-label">Informe o terceiro valor:</label>
+              <input type="number" id="x3" name="x3" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="x4" class="form-label">Informe o quarto valor:</label>
+              <input type="number" id="x4" name="x4" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="x5" class="form-label">Informe o quinto valor:</label>
+              <input type="number" id="x5" name="x5" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="x6" class="form-label">Informe o sexto valor:</label>
+              <input type="number" id="x6" name="x6" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="x7" class="form-label">Informe o sétimo valor:</label>
+              <input type="number" id="x7" name="x7" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
