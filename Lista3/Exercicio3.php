1969e0016dd8b0b1763e9626497f7116901a5333
Gerson <gersonscolari2@gmail.com>
2026-03-07 18:36:35 -0300

    Formatei os arquivos pois não estavam indentados coretamente.

--- Lista3/Exercicio3.php
+++ Lista3/Exercicio3.php
@@ -2,47 +2,44 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Atividade 3</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Atividade 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-  <div class="container py-3">
-    <h1>Exercício 3 - Ordem de A e B</h1>
-    <form method="post">
-      <div class="mb-3">
-        <label for="x1" class="form-label">Informe o valor de A:</label>
-        <input type="number" id="x1" name="x1" class="form-control" required="">
-      </div>
-      <div class="mb-3">
-        <label for="x2" class="form-label">Informe o valor de B:</label>
-        <input type="number" id="x2" name="x2" class="form-control" required="">
-      </div>
-      
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    <div class="container py-3">
+        <h1>Exercício 3 - Ordem de A e B</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="x1" class="form-label">Informe o valor de A:</label>
+                <input type="number" id="x1" name="x1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="x2" class="form-label">Informe o valor de B:</label>
+                <input type="number" id="x2" name="x2" class="form-control" required="">
+            </div>
 
-      $x1 = $_POST['x1'];
-      $x2 = $_POST['x2'];
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-      if ($x1 > $x2) {
-        echo "$x2 - $x1";
-      }
-      elseif ($x2 > $x1) {
-        echo "$x1 - $x2";
-      }
-      else {
-        echo "Valores iguais: $x1";
-      }
+            $x1 = $_POST['x1'];
+            $x2 = $_POST['x2'];
 
-    }
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-  </div>
+            if ($x1 > $x2) {
+                echo "$x2 - $x1";
+            } elseif ($x2 > $x1) {
+                echo "$x1 - $x2";
+            } else {
+                echo "Valores iguais: $x1";
+            }
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
 </body>
 
 </html>
--- Lista3/Exercicio4.php
+++ Lista3/Exercicio4.php
@@ -10,15 +10,11 @@
 
 <body>
     <div class="container py-3">
-        <h1>Exercício 2 - Soma de Valores ou Triplo da Soma (x1==x2)</h1>
+        <h1>Exercício 4 - Desconto produtos acima de R$100,00</h1>
         <form method="post">
             <div class="mb-3">
-                <label for="x1" class="form-label">Informe o primeiro valor:</label>
+                <label for="x1" class="form-label">Informe o valor do produto:</label>
                 <input type="number" id="x1" name="x1" class="form-control" required="">
-            </div>
-            <div class="mb-3">
-                <label for="x2" class="form-label">Informe o segundo valor:</label>
-                <input type="number" id="x2" name="x2" class="form-control" required="">
             </div>
 
             <button type="submit" class="btn btn-primary">Enviar</button>
@@ -27,15 +23,6 @@
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             $x1 = $_POST['x1'];
-            $x2 = $_POST['x2'];
-
-            if ($x1 != $x2) {
-                $resultado = $x1 + $x2;
-                echo "Valores diferentes! Resultado: $x1 + $x2 = $resultado";
-            } else {
-                $resultado = ($x1 + $x2) * 3;
-                echo "Valores Iguais! Resultado: ($x1 + $x2) * 3 = $resultado";
-            }
         }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
