5112bf0a8fabc9dd45a9290317eca4ad9b1e4724
Gerson <gersonscolari2@gmail.com>
2026-03-10 21:37:20 -0300

    Exercicio 5 - Atvidade 4

---
+++ Lista4/Exercicio5.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Atividade 4</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Atividade 4 - Calcular a raiz do (número)</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="x1" class="form-label">Informe um número: </label>
+              <input type="number" id="x1" name="x1" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $x1 = $_POST['x1'];
+
+    $raiz = sqrt($x1);
+    $raizformatada = number_format($raiz, 2, ",", ".");
+
+    echo "<br>A raiz quadrada de $x1 é $raizformatada";
+}
+
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
