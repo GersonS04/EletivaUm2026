681ffbcd4a40660d45afed2f3242e61734773afb
Gerson <gersonscolari2@gmail.com>
2026-03-15 16:39:25 -0300

    Exercicio 12 - Atividade 4

---
+++ Lista4/Exercicio12.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Atividade 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 12 - Senha Aleatória</h1>
+        <form method="post">
+            <button type="submit" class="btn btn-primary">Criar Senha</button>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+            $caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            $senha = substr(str_shuffle($caracteres), 0, 8);
+            echo "<p><br>$senha</p>";
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
