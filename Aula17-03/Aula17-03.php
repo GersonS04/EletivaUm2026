fae55fc32e8498aac866490eef02d12e1b519109
Gerson <gersonscolari2@gmail.com>
2026-03-17 20:15:01 -0300

    Aula 17-03

---
+++ Aula17-03/Aula17-03.php
@@ -0,0 +1,34 @@
+<?php
+    $mapa1 = array('Joao', 'Maria', 3);
+    print_r($mapa1);
+    echo "<p></p>";
+    var_dump($mapa1);
+    echo "Valor da posição 2".$mapa1[2]."</p>";
+
+    $mapa[1] = "Vanessa";
+    $mapa[2] = "José";
+    $mapa[3] = "Clara";
+    print_r($mapa2);
+
+    $contatos["Vanessa"] = "123456";
+    $contatos["José"] = "098765";
+    echo "<p></p>";
+    print_r($contatos);
+
+    foreach($contatos as $valor){
+        echo "<p>Telefone: $valor </p>";
+    }
+
+    foreach($contatos as $chave => $valor){
+        echo "<p>Telefone de $chave: $valor</p>";
+    }
+    //apagar posição
+    unset($mapa1[2]);
+    print_r($mapa1);
+
+    //funções
+    $quantidade = count($mapa2);
+    echo "<p>Quantidade elementos mapa 2: $quantidade</p>";
+    asort($contatos); //ordenar pelo valor
+    ksort($contatos); //ordenar pela chave
+?>
