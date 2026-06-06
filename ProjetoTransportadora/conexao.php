6cec84177d81e3f41ab33d07ec950e521e549a13
Gerson <gersonscolari2@gmail.com>
2026-06-06 12:52:37 -0300

    Ultima correção do projeto.

--- ProjetoTransportadora/conexao.php
+++ ProjetoTransportadora/conexao.php
@@ -7,8 +7,6 @@ $senha = "";
 try {
     $pdo = new PDO($dominio, $usuario, $senha);
 
-    // garante modo de erro (boa prática)
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die("Erro ao conectar ao banco: " . $e->getMessage());
 }
